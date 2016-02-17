e730924ba9b32690f295a102afe0907b5a9f181e
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-02-17 08:56:07 -0700

    fixed decimal currency issue

--- Test/test.js
+++ Test/test.js
@@ -1,22 +1,22 @@
 var code = require('../main');
 var expect = require('chai').expect;
 
-describe("Tax Calculator", function(num){
-  expect(code.calcTax(10)).to.equal(1);
-  });
+describe("calcTax", function(num){
+  expect(code.calcTax(10)).to.equal(1.00);
+
   it('should tax 10% on the first $10', function() {
-    expect(code.calcTax(1)).to.equal(0.1);
-    expect(code.calcTax(10)).to.equal(1);
+    expect(code.calcTax(1)).to.equal(0.10);
+    expect(code.calcTax(10)).to.equal(1.00);
   });
 
   it('should tax 7% on the second $10', function() {
     expect(code.calcTax(15)).to.equal(1.35);
-    expect(code.calcTax(20)).to.equal(1.7000000000000002);
+    expect(code.calcTax(20)).to.equal(1.70);
   });
 
   it('should tax 5% on the third $10', function() {
-    expect(code.calcTax(26)).to.equal(2);
-    expect(code.calcTax(30)).to.equal(2.2);
+    expect(code.calcTax(26)).to.equal(2.00);
+    expect(code.calcTax(30)).to.equal(2.20);
   });
 
   it('should tax 3% above $30', function() {
@@ -26,3 +26,5 @@ describe("Tax Calculator", function(num){
   it('return a string if num is below 0', function() {
     expect(code.calcTax(-20)).to.equal("Looks more like a refund to me.");
   });
+
+    });
--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 module.exports = {
 
-    calcTax: function(num) {
-       var tax = 0;
-       if(num > 0 && num <= 10) {
-         tax = num * 0.1;
-         return tax;
-        }
-       if (num > 10 && num <= 20) {
-         tax = 1 + ((num-10) * 0.07);
-         return tax;
-        }
-       if (num > 20 && num <= 30) {
-         tax = 1.7 + ((num-20) * 0.05);
-         return tax;
-       }
-       if (num > 30) {
-         tax = 2.2 + ((num-30) * 0.03);
-         return (tax);
-       }
-       if(num < 0) {
-         return "Looks more like a refund to me.";
-       }
-     }
+   calcTax: function(num) {
+     var tax = 0;
+     if(num > 0 && num <= 10) {
+       tax = (num * 0.1);
+       return Number(tax.toFixed(2));
+      }
+     if (num > 10 && num <= 20) {
+       tax = 1 + ((num-10) * 0.07);
+       return Number(tax.toFixed(2));
+      }
+     if (num > 20 && num <= 30) {
+       tax = 1.7 + ((num-20) * 0.05);
+       return Number(tax.toFixed(2));
+      }
+     if (num > 30) {
+       tax = 2.2 + ((num-30) * 0.03);
+       return Number(tax.toFixed(2));
+      }
+     if(num < 0) {
+       return "Looks more like a refund to me.";
+      }
+    }
 
   };
